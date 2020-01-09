2cddacedeebc2244d94911cdcc6b06e9de5cb470
at-oanhung <oanh.ung@asiantech.vn>
2020-01-09 09:15:16 +0700

    fix space

--- Js/Js1/Ex.js
+++ Js/Js1/Ex.js
@@ -1,18 +1,15 @@
-var a = 20;
-var b = 5;
+var a;
+var b;
+var c;
 
-console.log("a = " + a);
-console.log("b = " + b);
-
-function ex1() {
+function ex1(a, b) {
   if (a === b) {
     return 3 * (a + b);
   }
-  return (a + b);
+  return a + b;
 }
 
-function ex2() {
-  var c = 19;
+function ex2(a, c) {
   if (a > c) {
     return 3 * (a - c);
   }
@@ -34,16 +31,16 @@ function ex3(s) {
     }
   }
   
-  if (sum %3 === 0) {
-    arr = arr0.map( function(i) {
+  if (sum % 3 === 0) {
+    arr = arr0.map(function(i) {
       return s.replace("*", i)
     });
-  } else if (sum %3 === 1) {
-    arr = arr1.map( function(i) {
+  } else if (sum % 3 === 1) {
+    arr = arr1.map(function(i) {
       return s.replace("*", i)
     });
   } else {
-    arr = arr2.map( function(i) {
+    arr = arr2.map(function(i) {
       return s.replace("*", i)
     });
   }
@@ -54,12 +51,12 @@ function ex4(s) {
   var arr1 = [];
   var a = ex3(s);
   arr1 = a.filter(function(i) {
-    return Number(i.charAt(i.length - 1))%2 === 0;
+    return Number(i.charAt(i.length - 1)) % 2 === 0;
   });
   return arr1;
 }
 
-console.log("Ex1: " + ex1());
-console.log("Ex2: " + ex2());
-console.log("Ex3: " + ex3('1234567890*') );
-console.log("Ex4: " + ex4('1234567890*') );
+console.log("Ex1: " + ex1(20, 5));
+console.log("Ex2: " + ex2(20, 19));
+console.log("Ex3: " + ex3('1234567890*'));
+console.log("Ex4: " + ex4('1234567890*'));
