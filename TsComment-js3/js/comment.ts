45fc4254fe64a1be9a9e5bd5968af9d72a99f1cb
at-oanhung <oanh.ung@asiantech.vn>
2020-02-10 10:47:30 +0700

    fix convention

--- TsComment-js3/js/comment.ts
+++ TsComment-js3/js/comment.ts
@@ -1,52 +1,52 @@
 interface Comment {
-	id: number,
-	content: string,
-	isDelete: boolean,
-	idArticle: number,
-	user: {
-		id: number,
-		name: string,
-		avartar: string,
-	},
+  id: number,
+  content: string,
+  isDelete: boolean,
+  idArticle: number,
+  user: {
+    id: number,
+    name: string,
+    avartar: string,
+  },
 }
 
 let listCommentEl: HTMLElement = document.getElementById('js-list-comment');
 let comment: Comment[];
 function getCommentLs(): void {
-	comment= JSON.parse(localStorage.getItem('listComment'));
-	if (!comment) {
-		comment = [];
-	}
+  comment= JSON.parse(localStorage.getItem('listComment'));
+  if (!comment) {
+    comment = [];
+  }
 }
 
 function setCommentLs(): void {
-	localStorage.setItem('listComment', JSON.stringify(comment));
+  localStorage.setItem('listComment', JSON.stringify(comment));
 }
 
 function renderComment(): void {
-	getCommentLs();
+  getCommentLs();
 
-	//Handle
-	listCommentEl.innerHTML = '';
-	let contentEl: string = '';
-	if (comment.length) {
-		for (let i = 0; i < comment.length; i++) {
-			if (comment[i].isDelete == false) {
-				contentEl += '<li class="comment-item">' + 
-				'<a href="#" class="comment-item-avartar">' +
-				'<img src="' + comment[i].user.avartar + '" alt="Avartar">' +
-				'</a>' +
-				'<div class="comment-item-inf">' + 
-				'<span class="comment-item-username clr">' + comment[i].user.name + '</span>' +
-				'<span class="comment-item-cmt clr js-content">' + comment[i].content + '</span>' +
-				'<button type="button" class="btn-second delete js-delete-cm" data-id="' + comment[i].id +'" id="a">Delete</button>' +
-				'</div>' + 
-				'<li>';
-			}
-		}
-	}
-	listCommentEl.innerHTML = contentEl;
-	deleteComment();
+  //Handle
+  listCommentEl.innerHTML = '';
+  let contentEl: string = '';
+  if (comment.length) {
+    for (let i = 0; i < comment.length; i++) {
+      if (comment[i].isDelete == false) {
+        contentEl += '<li class="comment-item">' + 
+        '<a href="#" class="comment-item-avartar">' +
+        '<img src="' + comment[i].user.avartar + '" alt="Avartar">' +
+        '</a>' +
+        '<div class="comment-item-inf">' + 
+        '<span class="comment-item-username clr">' + comment[i].user.name + '</span>' +
+        '<span class="comment-item-cmt clr js-content">' + comment[i].content + '</span>' +
+        '<button type="button" class="btn-second delete js-delete-cm" data-id="' + comment[i].id +'" id="a">Delete</button>' +
+        '</div>' + 
+        '<li>';
+      }
+    }
+  }
+  listCommentEl.innerHTML = contentEl;
+  deleteComment();
 }
 
 renderComment();
@@ -59,34 +59,34 @@ renderComment();
 let addEl: HTMLElement = document.getElementById('js-add-cm');
 addEl.addEventListener('click', addComment);
 function addComment():void {
-	getCommentLs();
+  getCommentLs();
 
-	let contentCommentEl =  <HTMLInputElement>document.getElementById('js-input');
-	let contentComment: string = contentCommentEl.value.trim();
+  let contentCommentEl =  <HTMLInputElement>document.getElementById('js-input');
+  let contentComment: string = contentCommentEl.value.trim();
 
-	if (!contentComment) {
-		alert('You have not entered a comment. Error!'); 
-	} else {
-		let idComment: number = comment.length + 1;
-		let isDelete: boolean = false;
-		let idArticle: number = 1;
-		comment.push(<Comment>{
-			id: idComment,
-			content: contentComment, 
-			isDelete: isDelete, 
-			idArticle: idArticle,
-			user: {
-				id: 1,
-				name: 'Oanh Thuy',
-				avartar: 'images/avartar.png',
-			},
-		});
-	}
-	//Reset input value
-	contentCommentEl.value = '';
-	//setLs
-	setCommentLs();
-	renderComment();
+  if (!contentComment) {
+    alert('You have not entered a comment. Error!'); 
+  } else {
+    let idComment: number = comment.length + 1;
+    let isDelete: boolean = false;
+    let idArticle: number = 1;
+    comment.push(<Comment>{
+      id: idComment,
+      content: contentComment, 
+      isDelete: isDelete, 
+      idArticle: idArticle,
+      user: {
+        id: 1,
+        name: 'Oanh Thuy',
+        avartar: 'images/avartar.png',
+      },
+    });
+  }
+  //Reset input value
+  contentCommentEl.value = '';
+  //setLs
+  setCommentLs();
+  renderComment();
 }
 /*
 * click on "Delete" button 
@@ -95,20 +95,20 @@ function addComment():void {
 * take data contentComment - from input value
 */
 function deleteComment(): void {
-	getCommentLs();
-	let clickDelete = document.getElementsByClassName('js-delete-cm') as HTMLCollectionOf<HTMLElement>;
-	for (let i = 0; i < clickDelete.length; i++) {
-		clickDelete[i].addEventListener('click', function() {
-			let rowDe: number = + this.dataset.id;
-			for (let j = 0; j < comment.length; j++) {
-				if (rowDe === comment[j].id) {
-					comment[i].isDelete = true;
-					localStorage.setItem('listComment', JSON.stringify(comment));
-					alert('Delete sussces!');
-					renderComment();
-					break;
-				}
-			}
-		});
-	}
+  getCommentLs();
+  let clickDelete = document.getElementsByClassName('js-delete-cm') as HTMLCollectionOf<HTMLElement>;
+  for (let i = 0; i < clickDelete.length; i++) {
+    clickDelete[i].addEventListener('click', function() {
+      let rowDe: number = + this.dataset.id;
+      for (let j = 0; j < comment.length; j++) {
+        if (rowDe === comment[j].id) {
+          comment[i].isDelete = true;
+          localStorage.setItem('listComment', JSON.stringify(comment));
+          alert('Delete sussces!');
+          renderComment();
+          break;
+        }
+      }
+    });
+  }
 }
